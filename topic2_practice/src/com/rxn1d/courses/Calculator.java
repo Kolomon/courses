a35bedcbfbaea442d11b9f9bffea53aff4ff52e2
Kolomon <kolomiiets.vladyslav@gmail.com>
2016-02-29 18:50:23 +0200

    Калькулятор. Идею описал, нужно правильно дописать.

--- topic2_practice/src/com/rxn1d/courses/Calculator.java
+++ topic2_practice/src/com/rxn1d/courses/Calculator.java
@@ -7,10 +7,52 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("---> START Calculator application <---");
 
+        double numb1 = 0;
+        double numb2 = 0;
+        String[0] in = Double.parseDouble(ConsoleReader.readFromConsole());
+        String[0] in = double numb1;
+        String[1] in = o;
+        String[2] in = Double.parseDouble(ConsoleReader.readFromConsole());
+        String[2] in = double numb2;
+
+
+        if (o.equals("+")) {
+            System.out.println(numb1 + " + " + numb2 + " = " + add(numb1,numb2));
+            return;
+        }
+        if (o.equals("-")) {
+            System.out.println(numb1 + " + " + numb2 + " = " + sub(numb1,numb2));
+            return;
+        }
+        if (o.equals("*")) {
+            System.out.println(numb1 + " + " + numb2 + " = " + mul(numb1,numb2));
+            return;
+        }
+        if (o.equals("/")) {
+            System.out.println(numb1 + " + " + numb2 + " = " + div(numb1,numb2));
+            return;
+        }
+    }
+
+    public double add(double numb1, double numb2) {
+        return numb1 + numb2;
+    }
+
+    public double sub(double numb1, double numb2) {
+        return numb1 - numb2;
+    }
+
+    public double mul(double numb1, double numb2) {
+        return numb1 * numb2;
+    }
+
+    public double div(double numb1, double numb2) {
+        return numb1 / numb2;
+    }
+
         // для того, чтобы читать данные из консоли спользуйте данную конструкцию
         String[] in = ConsoleReader.readFromConsole();
         System.out.println("---> EXIT Calculator application <---");
-
 
     }
 }
