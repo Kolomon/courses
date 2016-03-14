63516d725e864e7578098952e7c2834808a1fc7b
Kolomon <kolomiiets.vladyslav@gmail.com>
2016-03-14 20:06:20 +0200

    1

--- topic8/src/main/java/com/courses/spalah/text/analyzer/TextAnalyzer.java
+++ topic8/src/main/java/com/courses/spalah/text/analyzer/TextAnalyzer.java
@@ -1,10 +1,35 @@
 package com.courses.spalah.text.analyzer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ievgen Tararaka
  */
 public class TextAnalyzer {
+    public static String[] readLineFromFile() {
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader("E:\\111\\courses\\topic8\\src\\main\\resources"));
+
+            String line;
+            List<String> lines = new ArrayList<String>();
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+//если нужен массив то список можно запросто преобрпзовать
+            String[] linesAsArray = lines.toArray(new String[lines.size()]);
+            return linesAsArray;
+        } catch (IOException e) {
+            e.getMessage();
+            System.out.println("CAN NOT READ FILE:");
+            return new String[]{""};
+        }
+    }
     public static void main(String[] args) {
-        // Входная точка для аналайзера текста
+
     }
 }
